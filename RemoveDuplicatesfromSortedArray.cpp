3815995e6214cb88612c3c4a3430fdd8f27a2a74
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-05-03 17:23:38 +0530

    DAY 1

--- RemoveDuplicatesfromSortedArray.cpp
+++ RemoveDuplicatesfromSortedArray.cpp
@@ -3,7 +3,6 @@ public:
     int removeDuplicates(vector<int>& nums) {
         
         int n=nums.size();
-        
         int i=0;
         for(int j=1;j<n;j++){
             if(nums[i]!=nums[j]){
