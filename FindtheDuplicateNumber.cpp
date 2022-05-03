009c3f467a2a4e3e399c465e2ea7c5183e16cc89
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-05-03 17:22:14 +0530

    DAY 1

--- FindtheDuplicateNumber.cpp
+++ FindtheDuplicateNumber.cpp
@@ -6,7 +6,6 @@ public:
         //we will use the indexes to mark the traversed elements negative
         //as soon as the value will come negative that is only possible when their will be a duplicate number
         int n = nums.size();
-
         for (int i = 0; i < n; i++)
         {
             int a = abs(nums[i]);
