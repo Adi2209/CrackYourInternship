81b16681a5ad0a53dfa5326e236b98d0be25466a
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-05-03 17:25:46 +0530

    DAY 1

--- SortColors.cpp
+++ SortColors.cpp
@@ -3,7 +3,6 @@ public:
     void sortColors(vector<int>& nums) {
         
         int low=0,mid=0,high=nums.size()-1;
-        
         while(mid<=high){
             if(nums[mid]==0){
                 swap(nums[low],nums[mid]);
